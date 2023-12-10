de7a48b9d378a0ec2eb9f8859d52f4e05e4e54e5
Tr00y2ooo <08005252a@gmail.com>
2023-12-11 01:42:33 +0800

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Fade-in animation for theme cards
+    const themeCards = document.querySelectorAll('.theme-card');
+
+    // Function to add 'show' class to elements after a delay
+    const showCards = () => {
+        themeCards.forEach((card, index) => {
+            setTimeout(() => {
+                card.classList.add('show');
+            }, index * 100); // Adjust the delay as needed
+        });
+    };
+
+    // Trigger the animation when the page is loaded
+    showCards();
+
+    // Example: Add a click event listener to note elements
+    const noteElements = document.querySelectorAll('.note');
+    noteElements.forEach(function (note) {
+        note.addEventListener('click', function () {
+            alert('Note clicked!'); // Replace with your desired behavior
+        });
+    });
+});
